2d2f6838b196e1a668be0f4b301be4da25f67529
Judit Ross <jtrs@incowia.com>
2016-05-10 14:03:06 +0200

    PLAT-492 : use inquirer version 1.0.2 + updated eslint configuration

--- tasks/cubx_webpackage_upload.js
+++ tasks/cubx_webpackage_upload.js
@@ -1,19 +1,19 @@
 /*global module,require*/
-'use strict'
-var Uploader = require('cubx-webpackage-uploader')
-var uploader = new Uploader()
-var inquirer = require('inquirer')
-var path = require('path')
+'use strict';
+var Uploader = require('cubx-webpackage-uploader');
+var uploader = new Uploader();
+var inquirer = require('inquirer');
+var path = require('path');
 
 module.exports = function (grunt) {
   // define an task starting with '+' if your task should be listed as one of the main top tasks
   grunt.registerTask('+webpackage-upload', 'Upload a Cubbles-Webpackage into a Cubbles-Base.', function () {
     // try to load the config
-    var webpackageConfigPath = grunt.config.get('activeWebpackageConfigPath')
-    var uploadConfigs = grunt.config.get('activeWebpackageConfig.uploadConfigs')
+    var webpackageConfigPath = grunt.config.get('activeWebpackageConfigPath');
+    var uploadConfigs = grunt.config.get('activeWebpackageConfig.uploadConfigs');
     if (uploadConfigs) {
       // Force into async mode and grab a handle to the "done" function.
-      var done = this.async()
+      var done = this.async();
       var doUpload = function (selectedConfig) {
         // create the config expected by the uploader
         var uploadConfig = {
@@ -23,40 +23,40 @@ module.exports = function (grunt) {
             proxy: selectedConfig.proxy
           },
           debug: selectedConfig.debug
-        }
+        };
         // run the upload
-        grunt.log.writeln('Starting upload ... ')
+        grunt.log.writeln('Starting upload ... ');
         uploader.uploadSingleWebpackage(uploadConfig, function (err, success) {
           if (err) {
             if (err.response && err.response.body) {
-              grunt.fail.fatal(new Error(JSON.stringify(err.response.body)))
-              done()
-              return
+              grunt.fail.fatal(new Error(JSON.stringify(err.response.body)));
+              done();
+              return;
             }
-            grunt.fail.fatal(err)
-            done()
+            grunt.fail.fatal(err);
+            done();
           } else {
-            grunt.log.ok(success)
-            done()
+            grunt.log.ok(success);
+            done();
           }
-        })
-      }
+        });
+      };
       // start user interaction
-      grunt.log.writeln('> Reading available configs from \'' + webpackageConfigPath + '\'')
-      var options = require('../lib/upload-options.js')(grunt, uploadConfigs)
-      inquirer.prompt(options.questions, function (result) {
+      grunt.log.writeln('> Reading available configs from \'' + webpackageConfigPath + '\'');
+      var options = require('../lib/upload-options.js')(grunt, uploadConfigs);
+      inquirer.prompt(options.questions).then(function (result) {
         // do upload, if user confirmed to do so
         if (result.selectedConfigKey !== 'CANCEL') {
-          doUpload(uploadConfigs[ result.selectedConfigKey ])
+          doUpload(uploadConfigs[ result.selectedConfigKey ]);
         } else {
-          done()
+          done();
         }
-      })
+      });
     } else {
       // create uploadConfig
-      grunt.log.warn('Upload configuration not found. Running a fix ... ')
-      var configFilePath = grunt.config.get('activeWebpackageConfigPath')
-      var workspaceConfig = grunt.file.readJSON(configFilePath)
+      grunt.log.warn('Upload configuration not found. Running a fix ... ');
+      var configFilePath = grunt.config.get('activeWebpackageConfigPath');
+      var workspaceConfig = grunt.file.readJSON(configFilePath);
       workspaceConfig.uploadConfigs = {
         dev: {
           url: 'https://www.cubbles.world/sandbox',
@@ -68,10 +68,10 @@ module.exports = function (grunt) {
           proxy: '',
           debug: false
         }
-      }
-      grunt.file.write(configFilePath, JSON.stringify(workspaceConfig, null, 2))
-      grunt.log.warn('Initial upload configuration now available at ' + configFilePath)
-      grunt.log.warn('Please review the config and retry ...')
+      };
+      grunt.file.write(configFilePath, JSON.stringify(workspaceConfig, null, 2));
+      grunt.log.warn('Initial upload configuration now available at ' + configFilePath);
+      grunt.log.warn('Please review the config and retry ...');
     }
-  })
-}
+  });
+};
